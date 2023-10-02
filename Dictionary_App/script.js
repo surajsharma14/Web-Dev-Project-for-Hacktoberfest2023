0a6ee64c98667abf600d613ba9182d91ba30657b
Akansh <akanshbende29@gmail.com>
2023-10-02 20:08:09 +0530

    New Api added for random background img generator

--- Dictionary_App/script.js
+++ Dictionary_App/script.js
@@ -4,7 +4,11 @@ const meaningContainerEl = document.getElementById("meaning-container");
 const titleEl = document.getElementById("title");
 const meaningEl = document.getElementById("meaning");
 const audioEl = document.getElementById("audio");
-const linkEl= document.getElementById("link");
+const linkEl = document.getElementById("link");
+
+// Added a random background generator So for every rendering it generare random image
+document.body.style.backgroundImage =
+  "url('https://source.unsplash.com/1600x900/?nature" + " ')";
 
 async function fetchAPI(word) {
   try {
@@ -15,8 +19,7 @@ async function fetchAPI(word) {
 
     const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
     const result = await fetch(url).then((response) => response.json());
-    inputEl.value="";
-
+    inputEl.value = "";
 
     if (result.title === "No Definitions Found") {
       infoEl.style.display = "none";
@@ -24,8 +27,7 @@ async function fetchAPI(word) {
       titleEl.innerText = word;
       audioEl.style.display = "none";
       meaningEl.innerText = "N/A";
-      linkEl.innerHTML=`Not Found`
-
+      linkEl.innerHTML = `Not Found`;
     } else {
       infoEl.style.display = "none";
       meaningContainerEl.style.display = "block";
@@ -33,19 +35,15 @@ async function fetchAPI(word) {
       meaningEl.innerText = result[0].meanings[0].definitions[0].definition;
       audioEl.style.display = "inline-flex";
       audioEl.src = result[0].phonetics[0].audio;
-      linkEl.innerHTML=`<a href=${result[0].sourceUrls[0]} target="_blank">Read More</a>`
-      
+      linkEl.innerHTML = `<a href=${result[0].sourceUrls[0]} target="_blank">Read More</a>`;
     }
-
   } catch (error) {
     infoEl.innerText = "An error occured, try again later";
   }
 }
 
-
 inputEl.addEventListener("keyup", (e) => {
   if (e.target.value && e.key === "Enter") {
     fetchAPI(e.target.value);
-
   }
 });
